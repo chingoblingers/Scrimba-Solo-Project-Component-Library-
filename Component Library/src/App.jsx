43106fce365124481ddb2d9ffadee1fbd40035ec
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-02-18 21:09:33 -0600

    imported vite, created component folders and started on logic and import of badge component

---
+++ Component Library/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Badge from './components/Badge/Badge.jsx'
+
+function App() {
+ 
+
+  return (
+    <Badge> Seal of approval </Badge>
+  )
+}
+
+export default App
