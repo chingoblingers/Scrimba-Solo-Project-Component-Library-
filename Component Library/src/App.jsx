be5c6a4953ec6fac47c47a822c1e9c79ddbf3117
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2026-02-23 20:44:44 -0600

    created Card component and card css and added margin to all components

--- Component Library/src/App.jsx
+++ Component Library/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Badge from './components/Badge/Badge.jsx'
 import Banner from './components/Banner/Banner.jsx'
+import Card from './components/Card/Card.jsx'
 
 function App() {
  
@@ -9,6 +10,7 @@ function App() {
     <>
     <Badge color="red" shape="bomb"> Lady Gaga </Badge>
     <Banner title="Too bad" status="fail"> Your verification process succeded and you are now able to make a banner component </Banner>
+    <Card title="Easy Development"> This is the begining of the best ever card that you will create ever. Dont miss out on this once in a lifetime chance! </Card>
     </>
   )
 }
---
+++ Component Library/src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+import AdvancedAnalyticsIcon from '@rsuite/icons/AdvancedAnalytics';
+
+export default function Card({children, title, ...rest}){
+    return(
+       <div className="card">
+        <div className='icon'>
+            <AdvancedAnalyticsIcon/>
+        </div>
+                <h2> {title} </h2>
+                <p> {children} </p>  
+    
+       </div> 
+    )
+}
